1b03e63f5fd206e1b3c818bc7d58652a390ff507
Adarsh Kumar <adarshkr@Adarshs-MacBook-Air.local>
2023-11-27 12:43:42 +0530

    Added controller and buildspec file

---
+++ spring-cicd-pipeline/src/main/java/com/codeops/springcicdpipeline/SpringCicdPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.codeops.springcicdpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCicdPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCicdPipelineApplication.class, args);
+	}
+
+}
---
+++ spring-cicd-pipeline/src/test/java/com/codeops/springcicdpipeline/SpringCicdPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeops.springcicdpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCicdPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
